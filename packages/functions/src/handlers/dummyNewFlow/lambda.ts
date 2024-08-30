8503d37a2c52175796ef4408f3b1d707eb770266
ghritak <ghritakjyotikalita@gmail.com>
2024-08-30 15:15:36 +0530

    nnew flow

--- packages/functions/src/handlers/dummyNewFlow/lambda.ts
+++ packages/functions/src/handlers/dummyNewFlow/lambda.ts
@@ -1,47 +1,65 @@
 import z from "zod";
 import { Config } from "sst/node/config";
-import apiHandler from "@/connector";
-import { getOffer } from "@/helpers/others/getOffer";
-import { findOrCreateCompanyFromOffer } from "@/helpers/others/findOrCreateCompanyFromOffer";
-import { createDealFromOffer } from "@/helpers/others/createDealFromOffer";
+import { apiHandler } from "@/connector";
+import { getsalesOrder } from "@/helpers/rackbeat/getSalesOrder";
+import { updateDealStage } from "@/helpers/hubspot/updateDealStage";
+import { getDeal } from "@/helpers/hubspot/getdeal";
+import { findOrCreateCompanyFromOrder } from "@/helpers/hubspot/findOrCreateCompanyFromOrder";
+import { createContactFromOrder } from "@/helpers/hubspot/createContactFromOrder";
 
 export const handler = apiHandler(
   {
-    workflowId: Number(Config.QUOTE_SENT_WORKFLOW_ID),
+    workflowId: Number(Config.SALES_ORDER_WORKFLOW_ID),
     parsers: {
       body: z.object({
-        number: z.number(),
+        key: z.number(),
       }),
     },
     referenceExtractor: ({
       parsed: {
-        body: { number },
+        body: { key },
       },
     }) => ({
-      id: "number",
-      value: String(number),
-      label: "Offer Number",
+      id: "key",
+      value: String(key),
+      label: "Order Number",
     }),
   },
   async ({ ctx, parsed: { body } }) => {
-    const number = String(body.number);
+    const number = String(body.key);
 
-    const offerData = await ctx.step(
-      { key: "get-rackbeat-offer-details" },
-      getOffer
+    const orderData = await ctx.step(
+      { key: "get-rackbeat-order-details" },
+      getsalesOrder
     )(number);
 
-    const company = await ctx.step(
-      { key: "find-or-create-company-in-hubspot" },
-      findOrCreateCompanyFromOffer
-    )(offerData);
+    if (orderData.order.offer_id) {
+      const deal = await ctx.step(
+        { key: "find-deal-in-hubspot" },
+        getDeal
+      )(orderData.order.offer_id);
 
-    const deal = await ctx.step(
-      { key: "create-deal-in-hubspot" },
-      createDealFromOffer
-    )(number, offerData, company.id);
+      const updatedDeal = await ctx.step(
+        { key: "update-deal-stage-in-hubspot" },
+        updateDealStage
+      )(deal.id, orderData);
+      ctx.logger.info(
+        { dealId: updatedDeal.id },
+        "Deal with Id updated successfully"
+      );
+    } else {
+      // No rackbeat offer id found, Creating contact and deal in Hubspot
+      const company = await ctx.step(
+        { key: "find-or-create-company-in-hubspot" },
+        findOrCreateCompanyFromOrder
+      )(orderData);
 
-    ctx.logger.info({ dealId: deal.id }, "Deal created successfully with Id");
+      const deal = await ctx.step(
+        { key: "create-deal-in-hubspot" },
+        createContactFromOrder
+      )(number, orderData, company.id);
+      ctx.logger.info({ dealId: deal.id }, "Deal created successfully with Id");
+    }
 
     return {
       statusCode: 200,
